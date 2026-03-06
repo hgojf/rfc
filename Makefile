320acc3663f4b53a7ed492dd5dbeebb331b35802
Henry Ford <fordhenry2299@gmail.com>
2026-03-06 23:02:53 +0000

    Generate a tags file to index the sections and ABNF rules defined in RFCs

--- Makefile
+++ Makefile
@@ -2,7 +2,14 @@
 
 PREFIX ?= /usr/local
 
-all:
+all: rfc.out rfctags
+
+rfc.out: rfc
+	sed 's|$${PREFIX}|${PREFIX}|g' < rfc > rfc.out
+
+rfctags: rfctags.o
+	$(CC) -o $@ $(LDFLAGS) rfctags.o
 
 install:
-	$(INSTALL) -m 0755 rfc ${PREFIX}/bin
+	$(INSTALL) -m 0755 rfc.out ${PREFIX}/bin/rfc
+	$(INSTALL) -m 0755 rfctags ${PREFIX}/libexec
---
+++ rfctags.c
@@ -0,0 +1,156 @@
+#include <sys/param.h>
+
+#include <err.h>
+#include <regex.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static void regdie(int, regex_t *, int, const char *, ...);
+static __dead void usage(void);
+
+/*
+ * Generates a ctags compatible file for an RFC/STD document, including
+ * section names and ABNF rule names as tags.
+ *
+ * This is written in C because I couldn't figure out how to make it
+ * fast enough as a shell script.
+ */
+int
+main(int argc, char *argv[])
+{
+	FILE *fp;
+	regex_t abnf, section;
+	size_t linesz;
+	ssize_t n;
+	int ch, error;
+	char *line;
+	const char *filename;
+
+	while ((ch = getopt(argc, argv, "")) != -1) {
+		switch (ch) {
+		default:
+			usage();
+		}
+	}
+
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 1)
+		usage();
+
+	filename = argv[0];
+
+	if ((fp = fopen(filename, "r")) == NULL)
+		err(1, "%s", filename);
+
+	if (pledge("stdio", NULL) == -1)
+		err(1, "pledge");
+
+	if ((error = regcomp(&abnf, "^[ \t]*([a-zA-Z][a-zA-Z0-9-]*)[ \t]+=[ \t]+.*$",
+	    REG_EXTENDED)) != 0)
+		regdie(error, &abnf, 1, "regcomp");
+	if ((error = regcomp(&section, "^([0-9]\\.)+[ \t]+([a-zA-Z \t]*)$", REG_EXTENDED)) != 0)
+		regdie(error, &section, 1, "regcomp");
+
+	line = NULL;
+	linesz = 0;
+	while ((n = getline(&line, &linesz, fp)) != -1) {
+		regmatch_t match[3];
+
+		if (n != 0 && line[n - 1] == '\n')
+			line[n - 1] = '\0';
+
+		if ((error = regexec(&abnf, line, nitems(match), match, 0)) != REG_NOMATCH) {
+			char *name;
+
+			if (error != 0)
+				regdie(error, &abnf, 1, "regexec");
+
+			if ((name = strndup(&line[match[1].rm_so],
+					    match[1].rm_eo - match[1].rm_so)) == NULL)
+				err(1, NULL);
+
+			if (printf("%s %s /^%s$/\n", name, filename,
+				   line) < 0)
+				err(1, "printf");
+
+			free(name);
+		}
+		else if ((error = regexec(&section, line, nitems(match), match, 0)) != REG_NOMATCH) {
+			char *section_name, *sp;
+
+			if (error != 0)
+				regdie(error, &section, 1, "regexec");
+
+			/*
+			 * This substitution extends to the end of the string
+			 * so we dont need to copy it.
+			 */
+			section_name = &line[match[2].rm_so];
+
+			/*
+			 * Tag names can't have whitespace in them, so use
+			 * underscores like mandoc(1) does.
+			 */
+			for (sp = section_name; *sp != '\0'; sp++)
+				if (*sp == ' ' || *sp == '\t')
+					*sp = '_';
+
+			if (printf("%s %s /^%s$/\n", section_name, filename,
+				   line) < 0)
+				err(1, "printf");
+		}
+	}
+	if (ferror(fp))
+		err(1, "getline");
+
+	fclose(fp);
+	free(line);
+	regfree(&abnf);
+	regfree(&section);
+}
+
+static void
+regdie(int error, regex_t *reg, int ex, const char *fmt, ...)
+{
+	size_t len;
+	va_list ap;
+	char buf[128], *bufp;
+
+	va_start(ap, fmt);
+
+	len = regerror(error, reg, buf, sizeof(buf));
+	bufp = buf;
+	if (len > sizeof(buf)) {
+		char *b;
+
+		/*
+		 * This gets leaked but we are dying anyways.
+		 */
+		if ((b = malloc(len)) != NULL) {
+			regerror(error, reg, b, len);
+			bufp = b;
+		}
+	}
+
+	fprintf(stderr, "%s", getprogname());
+
+	if (fmt != NULL) {
+		fprintf(stderr, ": ");
+		vfprintf(stderr, fmt, ap);
+	}
+
+	fprintf(stderr, ": %s\n", bufp);
+	exit(ex);
+}
+
+static void
+usage(void)
+{
+	fprintf(stderr, "usage: rfctags file\n");
+	exit(2);
+}
